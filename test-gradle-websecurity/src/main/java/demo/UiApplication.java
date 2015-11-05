655482b80e4ee0a090c8b51818ed7f69b1045b00
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2015-11-05 11:33:24 +0100

    enabled web security on rest

--- test-gradle-websecurity/src/main/java/demo/UiApplication.java
+++ test-gradle-websecurity/src/main/java/demo/UiApplication.java
@@ -4,9 +4,11 @@ package demo;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @ComponentScan
+@EnableGlobalMethodSecurity( securedEnabled = true )
 public class UiApplication {
 
 	public static void main(String[] args) {
