3210a2c4fdd6b034b2970ff0e57b1369de0ed0c7
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2015-11-16 17:34:50 +0100

    started implementing admin app

---
+++ admin-user-manager-app/src/main/java/demo/model/User.java
@@ -0,0 +1,69 @@
+package demo.model;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+ 
+    public User() {
+    }
+ 
+    public User(String name, String email, String password, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+ 
+    public Long getUserId() {
+        return userId;
+    }
+ 
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public String getEmail() {
+        return email;
+    }
+ 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+ 
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+    public String getRole() {
+        return role;
+    }
+ 
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ admin-user-manager-app/src/main/java/demo/rest/UserRest.java
@@ -0,0 +1,66 @@
+package demo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import demo.exception.NotFoundException;
+import demo.jpa.UsersJpa;
+import demo.model.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserRest {
+
+	@Autowired
+    private UsersJpa users;
+	
+	@Secured({"ROLE_ADMIN"})
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ResponseEntity<?> createUser(@RequestBody User user) {
+		user.setUserId(null);
+		user = users.save(user);
+		
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setLocation(ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(user.getUserId()).toUri());
+		return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@RequestMapping("/{userId}")
+	public User getUser(@PathVariable Long userId)
+	{
+		User u = users.findOne(userId);
+		if(u == null)
+		{
+			throw new NotFoundException();
+		}
+		
+		return u;
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@RequestMapping()
+	public List<User> getUsers() 
+	{
+		return users.findAll();
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@RequestMapping("/count")
+	public Long count() {
+		return users.count();
+	}
+}
