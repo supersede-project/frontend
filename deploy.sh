72e846cbe47f6938a5b85df2c4f845c2387d724f
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2015-12-17 17:48:56 +0100

    added deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+$CATALINA_HOME/bin/shutdown.sh ;
+
+rm -rf services/apache-tomcat-8.0.28/webapps/* ;
+
+cp wp5/build/libs/wp5-0.0.1-SNAPSHOT.war $CATALINA_HOME/webapps/ROOT.war ;
+
+cp wp5-test-app/build/libs/wp5-test-app-0.0.1-SNAPSHOT.war $CATALINA_HOME/webapps/wp5-test-app.war ;
+
+$CATALINA_HOME/bin/startup.sh ;
