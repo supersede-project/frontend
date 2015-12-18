0b1d4d50a02861abd493c2ec37772073817df6e3
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2015-12-18 16:09:16 +0100

    fixed env var

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-$CATALINA_HOME/bin/shutdown.sh ;
+$TOMCAT/bin/shutdown.sh ;
 
-rm -rf $CATALINA_HOME/webapps/* ;
+rm -rf $TOMCAT/webapps/* ;
 
-cp wp5/build/libs/wp5-0.0.1-SNAPSHOT.war $CATALINA_HOME/webapps/ROOT.war ;
+cp wp5/build/libs/wp5-0.0.1-SNAPSHOT.war $TOMCAT/webapps/ROOT.war ;
 
-cp admin-user-manager-app/build/libs/admin-user-manager-app-0.0.1-SNAPSHOT.war $CATALINA_HOME/webapps/admin-user-manager-app.war ;
+cp admin-user-manager-app/build/libs/admin-user-manager-app-0.0.1-SNAPSHOT.war $TOMCAT/webapps/admin-user-manager-app.war ;
 
-$CATALINA_HOME/bin/startup.sh ;
+$TOMCAT/bin/startup.sh ;
 
-echo $CATALINA_HOME
+echo $TOMCAT
