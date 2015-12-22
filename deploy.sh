5043ad493da78cc61c32886c29c6f0fd7b49db94
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2015-12-22 10:58:46 +0100

    hope this can fix deploy from jenkins

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ cp wp5/build/libs/wp5-0.0.1-SNAPSHOT.war $TOMCAT/webapps/ROOT.war
 
 cp admin-user-manager-app/build/libs/admin-user-manager-app-0.0.1-SNAPSHOT.war $TOMCAT/webapps/admin-user-manager-app.war
 
-$TOMCAT/bin/startup.sh &
+BUILD_ID=dontKillMe nohup $TOMCAT/bin/startup.sh &
+
+sleep 3
 
 echo $TOMCAT
