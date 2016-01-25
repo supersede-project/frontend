cca14f7da17c8ae5440c5ed3da9b915f58367fd8
Andrea Sosi <sosi.andrea@gmail.com>
2016-01-25 18:40:30 +0100

    - some changes for judgeAct

--- applications/game-requirements/src/main/java/demo/rest/JudgeActRest.java
+++ applications/game-requirements/src/main/java/demo/rest/JudgeActRest.java
@@ -4,12 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import demo.jpa.JudgeActsJpa;
+import demo.jpa.RequirementsMatricesDataJpa;
+import demo.jpa.UsersJpa;
 import demo.model.JudgeAct;
+import demo.model.RequirementsMatrixData;
+import demo.model.User;
+import eu.supersede.fe.exception.NotFoundException;
+import eu.supersede.fe.security.DatabaseUser;
 
 @RestController
 @RequestMapping("/judgeact")
@@ -18,11 +26,51 @@ public class JudgeActRest {
 	@Autowired
     private JudgeActsJpa judgeActs;
 	
+	@Autowired
+    private UsersJpa users;
+
+	@Autowired
+    private RequirementsMatricesDataJpa requirementsMatricesData;
+	
 	// get all the judgeActs if user is a judge
 	@PreAuthorize("hasRole('JUDGE')")
 	@RequestMapping(value = "",  method = RequestMethod.GET)
 	public List<JudgeAct> getJudgeActs() {
 
 		return judgeActs.findAll();
+	}
+	
+	// get a specific judgeAct 
+	@PreAuthorize("hasRole('JUDGE')")
+	@RequestMapping(value = "/{judgeActId}", method = RequestMethod.GET)
+	public JudgeAct getJudgeAct(@PathVariable Long judgeActId){	
+				
+		JudgeAct ja = judgeActs.findOne(judgeActId);
+		
+		if(ja == null)
+		{
+			throw new NotFoundException();
+		}
+		
+		return ja;
+	}
+	
+	// set the vote for of a judge in his/her judgeAct
+	@PreAuthorize("hasRole('JUDGE')")
+	@RequestMapping(method = RequestMethod.PUT, value="/{judgeActId}/vote/{vote}")
+	public void setjudgeActVote(Authentication authentication, @PathVariable Long judgeActId, @PathVariable Long vote){	
+		
+		DatabaseUser currentUser = (DatabaseUser) authentication.getPrincipal();
+		User judge = users.findOne(currentUser.getUserId());
+		
+		JudgeAct judgeAct = judgeActs.findOne(judgeActId);
+		
+		judgeAct.setJudge(judge);
+		judgeAct.setVoted(true);
+		judgeActs.save(judgeAct);
+		
+		RequirementsMatrixData requirementsMatrixData = judgeAct.getRequirementsMatrixData();		
+		requirementsMatrixData.setValue(vote);
+		requirementsMatricesData.save(requirementsMatrixData);
 	}
 }
--- applications/game-requirements/src/main/resources/static/js/game.js
+++ applications/game-requirements/src/main/resources/static/js/game.js
@@ -365,7 +365,7 @@ app.controllerProvider.register('vote_view', function($scope, $http, $location) 
 		});
 	 
 	 $scope.insertPlayerVote = function(playerVote){
-		 $http.put('game-requirements/playermove/' + $scope.playerMoveId + '/vote/' +playerVote)
+		 $http.put('game-requirements/playermove/' + $scope.playerMoveId + '/vote/' + playerVote)
 	    	.success(function(data) {
 	    		
     	});
@@ -384,4 +384,33 @@ app.controllerProvider.register('judge', function($scope, $http) {
 		}
 	});
 	
+});
+
+app.controllerProvider.register('judge_act', function($scope, $http, $location) {
+    
+	$scope.judgeActId = $location.search()['judgeActId'];
+    $scope.judgeAct = null;
+	$scope.requirementsChoices = [];
+	$scope.selectedRequirementsChoice = {selected:4};
+	
+	$http.get('game-requirements/judgeact/' + $scope.judgeActId)
+	.success(function(data) {
+		$scope.judgeAct = data;
+	});
+    
+	 $http.get('game-requirements/requirementchoice')
+		.success(function(data) {
+			$scope.requirementsChoices.length = 0;
+			for(var i = 0; i < data.length; i++)
+			{
+				$scope.requirementsChoices.push(data[i]);
+			}
+		});
+	 
+	 $scope.insertjudgeVote = function(judgeVote){
+		 $http.put('game-requirements/judgeact/' + $scope.judgeActId + '/vote/' + judgeVote)
+	    	.success(function(data) {
+	    		
+    	});
+	 };
 });
