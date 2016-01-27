d2126ae61fabfb53e40dee0a607b92683b8715b6
Andrea Sosi <sosi.andrea@gmail.com>
2016-01-27 14:18:32 +0100

    - minor changes

--- applications/game-requirements/src/main/resources/static/js/game.js
+++ applications/game-requirements/src/main/resources/static/js/game.js
@@ -1,6 +1,13 @@
 var app = angular.module('w5app');
 
-app.controllerProvider.register('game', function($scope, $http) {
+app.controllerProvider.register('game', function($scope, $http, $location) {
 	
+	$scope.gameId = $location.search()['gameId'];
+	$scope.game = null;
+	
+	$http.get('game-requirements/game/' + $scope.gameId)
+	.success(function(data) {
+		$scope.game = data;
+	});
 	
 });
