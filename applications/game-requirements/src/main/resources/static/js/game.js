cde9b19cf8ecc8c92bfb63c91d73c9f98f3552d5
Andrea Sosi <sosi.andrea@gmail.com>
2016-01-27 17:06:27 +0100

    - implement game_page - implement the connection with AHP alghoritm (to control) - some changes

--- applications/game-requirements/src/main/resources/static/js/game.js
+++ applications/game-requirements/src/main/resources/static/js/game.js
@@ -4,10 +4,18 @@ app.controllerProvider.register('game', function($scope, $http, $location) {
 	
 	$scope.gameId = $location.search()['gameId'];
 	$scope.game = null;
+	$scope.ahpResult = null;
 	
 	$http.get('game-requirements/game/' + $scope.gameId)
 	.success(function(data) {
 		$scope.game = data;
 	});
+	
+	$scope.computeAHP = function(gameId){
+		$http.get('game-requirements/ahp/' + gameId)
+		.success(function(data) {
+			$scope.ahpResult = data;
+		});
+	}
 	
 });
