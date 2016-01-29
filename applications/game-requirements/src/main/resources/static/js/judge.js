bc8e337ad755fd6b1ec8fc67e7e54148c8067183
Andrea Sosi <sosi.andrea@gmail.com>
2016-01-29 15:51:30 +0100

    - added vote in player_moves and judge_acts page - minor changes

--- applications/game-requirements/src/main/resources/static/js/judge.js
+++ applications/game-requirements/src/main/resources/static/js/judge.js
@@ -3,6 +3,7 @@ var app = angular.module('w5app');
 app.controllerProvider.register('judge', function($scope, $http) {
     
     $scope.judgeActs = [];
+	$scope.requirementsChoices = [];
     
     $http.get('game-requirements/judgeact')
 	.success(function(data) {
@@ -11,5 +12,27 @@ app.controllerProvider.register('judge', function($scope, $http) {
 			$scope.judgeActs.push(data[i]);
 		}
 	});
-	
+    
+	 $http.get('game-requirements/requirementchoice')
+		.success(function(data) {
+			$scope.requirementsChoices.length = 0;
+			for(var i = 0; i < data.length; i++)
+			{
+				$scope.requirementsChoices.push(data[i]);
+			}
+		});
+    
+	 $scope.setVote = function(judgeVote, judgeActId){
+		 $http.put('game-requirements/judgeact/' + judgeActId + '/vote/' + judgeVote)
+	    	.success(function(data) {
+	    		for(var i = 0; i < $scope.judgeActs.length; i++)
+				{
+					if($scope.judgeActs[i].judgeActId == judgeActId)
+					{
+						$scope.judgeActs[i].voted = true;
+					}
+				}
+    	});
+	 };
+
 });
--- applications/game-requirements/src/main/resources/static/js/player_moves.js
+++ applications/game-requirements/src/main/resources/static/js/player_moves.js
@@ -3,6 +3,7 @@ var app = angular.module('w5app');
 app.controllerProvider.register('player_moves', function($scope, $http) {
     
     $scope.playerMoves = [];
+	$scope.requirementsChoices = [];
     
     $http.get('game-requirements/playermove')
 	.success(function(data) {
@@ -12,4 +13,26 @@ app.controllerProvider.register('player_moves', function($scope, $http) {
 		}
 	});
 	
+	 $http.get('game-requirements/requirementchoice')
+		.success(function(data) {
+			$scope.requirementsChoices.length = 0;
+			for(var i = 0; i < data.length; i++)
+			{
+				$scope.requirementsChoices.push(data[i]);
+			}
+		});
+	 
+	 $scope.setVote = function(playerVote, playerMoveId){
+		 $http.put('game-requirements/playermove/' + playerMoveId + '/vote/' + playerVote)
+	    	.success(function(data) {
+	    		for(var i = 0; i < $scope.playerMoves.length; i++)
+				{
+					if($scope.playerMoves[i].playerMoveId == playerMoveId)
+					{
+						$scope.playerMoves[i].played = true;
+					}
+				}
+    	});
+	 };
+    
 });
