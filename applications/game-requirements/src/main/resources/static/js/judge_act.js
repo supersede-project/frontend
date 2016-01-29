63381287080724249691296cef15adbb533fc088
Andrea Sosi <sosi.andrea@gmail.com>
2016-01-29 11:28:28 +0100

    - some UI changes - list of players with their votes in judge act page

--- applications/game-requirements/src/main/resources/static/js/judge_act.js
+++ applications/game-requirements/src/main/resources/static/js/judge_act.js
@@ -3,13 +3,22 @@ var app = angular.module('w5app');
 app.controllerProvider.register('judge_act', function($scope, $http, $location) {
     
 	$scope.judgeActId = $location.search()['judgeActId'];
-    $scope.judgeAct = null;
+    $scope.judgeAct = undefined;
 	$scope.requirementsChoices = [];
 	$scope.selectedRequirementsChoice = {selected:4};
+	$scope.playerMoves = [];
 	
 	$http.get('game-requirements/judgeact/' + $scope.judgeActId)
 	.success(function(data) {
 		$scope.judgeAct = data;
+		
+		 $http.get('game-requirements/playermove/requirementsmatrixdata/' + $scope.judgeAct.requirementsMatrixData.requirementsMatrixDataId)
+			.success(function(data) {
+				for(var i = 0; i < data.length; i++)
+				{
+					$scope.playerMoves.push(data[i]);
+				}
+			});
 	});
     
 	 $http.get('game-requirements/requirementchoice')
@@ -26,5 +35,5 @@ app.controllerProvider.register('judge_act', function($scope, $http, $location) 
 	    	.success(function(data) {
 	    		
     	});
-	 };
+	 };	
 });
