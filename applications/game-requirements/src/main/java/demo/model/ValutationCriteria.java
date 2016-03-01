fd4a23389f04eca8c31ba1d650593d8db4c6016a
Andrea Sosi <sosi.andrea@gmail.com>
2016-03-01 16:43:41 +0100

    - criteria description

--- applications/game-requirements/src/main/java/demo/model/ValutationCriteria.java
+++ applications/game-requirements/src/main/java/demo/model/ValutationCriteria.java
@@ -22,6 +22,7 @@ public class ValutationCriteria {
 	@GeneratedValue(strategy = GenerationType.AUTO)
     private Long criteriaId;
     private String name;
+    private String description;
     
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "valutationCriteria")
 	private List<UserCriteriaPoint> userCriteriaPoints;
@@ -48,7 +49,15 @@ public class ValutationCriteria {
     public void setName(String name) {
         this.name = name;
     }
-    
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
     @JsonIgnore
     public List<UserCriteriaPoint> getUserCriteriaPoints(){
     	return userCriteriaPoints;
