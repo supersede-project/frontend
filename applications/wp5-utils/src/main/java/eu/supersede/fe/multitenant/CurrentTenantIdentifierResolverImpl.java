f648faa4461154ff0ede4eda3659582bd11123b9
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2016-03-08 17:13:11 +0100

    added log

--- applications/wp5-utils/src/main/java/eu/supersede/fe/multitenant/CurrentTenantIdentifierResolverImpl.java
+++ applications/wp5-utils/src/main/java/eu/supersede/fe/multitenant/CurrentTenantIdentifierResolverImpl.java
@@ -1,6 +1,8 @@
 package eu.supersede.fe.multitenant;
 
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,8 @@ import org.springframework.web.context.request.RequestContextHolder;
 @PropertySource("file:../conf/multitenancy.properties")
 public class CurrentTenantIdentifierResolverImpl implements CurrentTenantIdentifierResolver {
 
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Value("${application.multitenancy.default}")
 	private String DEFAULT_TENANT_ID;
 	
@@ -22,9 +26,13 @@ public class CurrentTenantIdentifierResolverImpl implements CurrentTenantIdentif
 		{
 			String identifier = (String) requestAttributes.getAttribute("CURRENT_TENANT_IDENTIFIER", RequestAttributes.SCOPE_REQUEST);
 			if (identifier != null) {
+				
+				log.debug("returning tenant: " + identifier);
 				return identifier;
 			}
 		}
+		
+		log.debug("returning default tenant: " + DEFAULT_TENANT_ID);
 		return DEFAULT_TENANT_ID;
 	}
 
--- applications/wp5-utils/src/main/java/eu/supersede/fe/multitenant/MultiTenancyInterceptor.java
+++ applications/wp5-utils/src/main/java/eu/supersede/fe/multitenant/MultiTenancyInterceptor.java
@@ -3,12 +3,16 @@ package eu.supersede.fe.multitenant;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import eu.supersede.fe.security.DatabaseUser;
 
 public class MultiTenancyInterceptor extends HandlerInterceptorAdapter {
+	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception 
@@ -37,6 +41,7 @@ public class MultiTenancyInterceptor extends HandlerInterceptorAdapter {
 		
 		if(multiTenantId != null)
 		{
+			log.debug("setting tenant: " + multiTenantId);
 			req.setAttribute("CURRENT_TENANT_IDENTIFIER", multiTenantId);
 		}
 		return true;
