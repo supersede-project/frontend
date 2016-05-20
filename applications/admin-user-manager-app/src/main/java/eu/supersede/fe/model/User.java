903d8ca8a49558fbf72c46cb75c3cf8ccc83cbd3
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2016-05-20 15:18:00 +0200

    restore user and profile, and jpas

--- applications/admin-user-manager-app/src/main/java/eu/supersede/fe/model/User.java
+++ applications/admin-user-manager-app/src/main/java/eu/supersede/fe/model/User.java
@@ -8,45 +8,69 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
-	
+
 	@Id
-    private Long userId;
-    private String name;
-    private String email;
- 
-    public User() {
-    }
- 
-    public User(Long userId, String name, String email, String password) {
-    	this.userId = userId;
-    	this.name = name;
-        this.email = email;
-    }
- 
-    public Long getUserId() {
-        return userId;
-    }
- 
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
- 
-    public String getName() {
-        return name;
-    }
- 
-    public void setName(String name) {
-        this.name = name;
-    }
- 
-    public String getEmail() {
-        return email;
-    }
- 
-    public void setEmail(String email) {
-        this.email = email;
-    }
- 
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long userId;
+	private String name;
+	private String email;
+	private String password;
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "users_profiles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "profile_id"))
+	private List<Profile> profiles;
+
+	public User() {
+	}
+
+	public User(String name, String email, String password, List<Profile> profiles) {
+		this.name = name;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.profiles = profiles;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@JsonIgnore
+	public String getPassword() {
+		return password;
+	}
+
+	@JsonProperty("password")
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Profile> getProfiles() {
+		return profiles;
+	}
+
+	public void setProfiles(List<Profile> profiles) {
+		this.profiles = profiles;
+	}
 }
--- applications/admin-user-manager-app/src/main/java/eu/supersede/fe/rest/ProfileRest.java
+++ applications/admin-user-manager-app/src/main/java/eu/supersede/fe/rest/ProfileRest.java
@@ -3,26 +3,22 @@ package eu.supersede.fe.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import eu.supersede.fe.integration.ProxyWrapper;
-import eu.supersede.fe.security.DatabaseUser;
-import eu.supersede.integration.api.datastore.fe.types.Profile;
+import eu.supersede.fe.jpa.ProfilesJpa;
+import eu.supersede.fe.model.Profile;
 
 @RestController
 @RequestMapping("/profile")
 public class ProfileRest {
 
 	@Autowired
-	private ProxyWrapper proxy;
+	private ProfilesJpa profiles;
 	
 	//@Secured({"ROLE_ADMIN"})
 	@RequestMapping("")
-	public List<Profile> getProfiles(Authentication authentication) {
-		DatabaseUser currentUser = (DatabaseUser) authentication.getPrincipal();
-		List<Profile> profiles = proxy.getFEDataStoreProxy().getProfiles(currentUser.getTenantId(), currentUser.getToken());
-		return profiles;
+	public List<Profile> getProfiles() {
+		return profiles.findAll();
 	}
 }
