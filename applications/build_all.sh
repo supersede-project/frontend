f09dc8be499f25a14db2ee4831492f1551ae978f
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2016-06-29 14:03:59 +0200

    added build_all script

---
+++ applications/build_all.sh
@@ -0,0 +1,5 @@
+cd supersede-frontend-core/ && gradle build && 
+cd ../supersede-client && gradle build && 
+cd ../supersede-frontend && gradle build && 
+cd ../admin-user-manager-app && gradle build && 
+cd ..
