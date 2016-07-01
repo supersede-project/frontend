361c4de3efd42b02b282016f4e234c01a7c7a8e2
Michele Lunelli <michele.lunelli@deltainformatica.eu>
2016-07-01 10:41:59 +0200

    fixed check logged user

--- applications/supersede-client/src/main/java/eu/supersede/fe/notification/NotificationUtil.java
+++ applications/supersede-client/src/main/java/eu/supersede/fe/notification/NotificationUtil.java
@@ -42,7 +42,7 @@ public class NotificationUtil {
 		if(securityContext != null)
 		{
 			Authentication authentication = securityContext.getAuthentication();
-			if(authentication != null)
+			if(authentication != null && authentication instanceof DatabaseUser)
 			{
 				DatabaseUser userDetails = (DatabaseUser)authentication.getPrincipal();
 				tenant = userDetails.getTenantId();
