fd92e01e5321a1d0b1dc464527750697b18ecd48
Mauro Fruet <mauro.fruet@deltainformatica.eu>
2016-10-24 09:02:16 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,10 @@ sleep 3
 
 rm -rf $TOMCAT/webapps/*
 
-cp applications/wp5/build/libs/wp5-0.0.1-SNAPSHOT.war $TOMCAT/webapps/ROOT.war
-
-cp applications/admin-user-manager-app/build/libs/admin-user-manager-app-0.0.1-SNAPSHOT.war $TOMCAT/webapps/admin-user-manager-app.war
+cp applications/supersede-frontend/build/libs/supersede-frontend-0.0.3-SNAPSHOT.war $TOMCAT/webapps/ROOT.war
+cp applications/admin-user-manager-app/build/libs/admin-user-manager-app-0.0.3-SNAPSHOT.war $TOMCAT/webapps/admin-user-manager-app.war
+cp applications/supersede-client/build/libs/supersede-client-0.0.3-SNAPSHOT.war $TOMCAT/webapps/supersede-client.war
+cp applications/supersede-frontend-core/build/libs/supersede-frontend-core-0.0.3-SNAPSHOT.war $TOMCAT/webapps/supersede-frontend-core.war
 
 BUILD_ID=dontKillMe nohup $TOMCAT/bin/startup.sh &
 
