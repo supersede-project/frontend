b85b9e99670dd455c1dc5cdb359c44b3d14058bd
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2016-11-29 16:01:45 +0100

    Gradle: clean before building

--- applications/build_all.sh
+++ applications/build_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd supersede-frontend-core/ && ./gradlew build &&
-cd ../supersede-frontend && ./gradlew build &&
-cd ../supersede-client && ./gradlew build &&
-cd ../admin-user-manager-app && ./gradlew build &&
-cd ../redis-sessions-inspector && ./gradlew build
+cd supersede-frontend-core/ && ./gradlew clean build &&
+cd ../supersede-frontend && ./gradlew clean build &&
+cd ../supersede-client && ./gradlew clean build &&
+cd ../admin-user-manager-app && ./gradlew clean build &&
+cd ../redis-sessions-inspector && ./gradlew clean build
