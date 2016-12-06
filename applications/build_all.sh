6ed89d47e30eff779d0fbb0a61e0617c25c13d76
Mauro Fruet <mauro.fruet@deltainformatica.eu>
2016-12-06 16:20:35 +0100

    force refresh dependencies in build script

--- applications/build_all.sh
+++ applications/build_all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo -e "\nBuilding supersede-frontend-core...\n"
-cd supersede-frontend-core/ && ./gradlew build
+cd supersede-frontend-core/ && ./gradlew build --refresh-dependencies
 
 echo -e "\nBuilding supersede-frontend...\n"
-cd ../supersede-frontend && ./gradlew build
+cd ../supersede-frontend && ./gradlew build --refresh-dependencies
 
 echo -e "\nBuilding supersede-client...\n"
-cd ../supersede-client && ./gradlew build
+cd ../supersede-client && ./gradlew build --refresh-dependencies
 
 echo -e "\nBuilding admin-user-manager-app...\n"
-cd ../admin-user-manager-app && ./gradlew build
+cd ../admin-user-manager-app && ./gradlew build --refresh-dependencies
 
 echo -e "\nBuilding redis-sessions-inspector...\n"
-cd ../redis-sessions-inspector && ./gradlew build
+cd ../redis-sessions-inspector && ./gradlew build --refresh-dependencies
