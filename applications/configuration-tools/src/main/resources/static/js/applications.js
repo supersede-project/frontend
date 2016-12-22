e4d6db33f743a847e681f1a12b3de384512af7a7
Mauro Fruet <mauro.fruet@deltainformatica.eu>
2016-12-22 12:58:16 +0100

    fill the application list page

--- applications/configuration-tools/src/main/resources/static/js/applications.js
+++ applications/configuration-tools/src/main/resources/static/js/applications.js
@@ -1,6 +1,7 @@
 var app = angular.module('w5app');
 app.controllerProvider.register('applications', function($scope, $http) {
     $scope.session_id = "";
+    $scope.applications = {};
 
     $scope.getSession = function() {
         $http({
@@ -13,5 +14,19 @@ app.controllerProvider.register('applications', function($scope, $http) {
             });
     };
 
+    $scope.getApplications = function() {
+        $http({
+            url: "supersede-first-app/applicationlist",
+            method: 'GET'
+        }).success(function(data) {
+            console.log("Applications:");
+            console.log(data);
+            $scope.applications = data;
+            }).error(function(err) {
+                console.log(err);
+            });
+    };
+
     $scope.getSession();
+    $scope.getApplications();
 });
