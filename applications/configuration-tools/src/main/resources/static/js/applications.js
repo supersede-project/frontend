7b5fa49ff0b3b4b24dd4b6828fdc025501318ac2
Mauro Fruet <mauro.fruet@deltainformatica.eu>
2016-12-22 11:00:45 +0100

    show session id in applications page

--- applications/configuration-tools/src/main/resources/static/js/applications.js
+++ applications/configuration-tools/src/main/resources/static/js/applications.js
@@ -1,13 +1,13 @@
 var app = angular.module('w5app');
 app.controllerProvider.register('applications', function($scope, $http) {
-    $scope.session = "";
+    $scope.session_id = "";
 
     $scope.getSession = function() {
         $http({
             url: "supersede-first-app/session",
             method: 'GET'
         }).success(function(data) {
-            $scope.session = data;
+            $scope.session_id = data.id;
             }).error(function(err) {
                 console.log(err);
             });
