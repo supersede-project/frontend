e5c36ac119e1af78a3fa0cada1c6cbac7df040a4
Mauro Fruet <mauro.fruet@deltainformatica.eu>
2017-01-18 15:15:10 +0100

    update dependencies, do not refresh dependencies in build all script

--- applications/build_all.sh
+++ applications/build_all.sh
@@ -2,20 +2,20 @@
 
 cd supersede-frontend-core
 echo -e "\nBuilding supersede-frontend-core...\n"
-./gradlew build --refresh-dependencies
+./gradlew build
 
 cd ../supersede-frontend
 echo -e "\nBuilding supersede-frontend...\n"
-./gradlew build --refresh-dependencies
+./gradlew build
 
 cd ../supersede-client
 echo -e "\nBuilding supersede-client...\n"
-./gradlew build --refresh-dependencies
+./gradlew build
 
 cd ../admin-user-manager-app
 echo -e "\nBuilding admin-user-manager-app...\n"
-./gradlew build --refresh-dependencies
+./gradlew build
 
 cd ../configuration-tools
 echo -e "\nBuilding configuration-tools...\n"
-./gradlew build --refresh-dependencies
+./gradlew build
